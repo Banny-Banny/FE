4e8c57ba004f3bcaae336e20b6f045f8f26277df
Daram0o0 <c30728535@gmail.com>
2025-12-29 21:07:10 +0900

    feat(map): 카카오 로컬 API를 통한 주소 변환 기능 구현
    - useCurrentLocationAddress hook 추가
    - 위도/경도를 행정 구역 주소로 변환하는 기능 구현
    - 카카오 로컬 API (coord2address) 연동

---
+++ components/map/components/current-location/hooks/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Current Location Hooks
+ * Version: 1.0.0
+ * Created: 2025-01-XX
+ *
+ * 현재 위치 관련 hooks 모음
+ */
+
+export { useCurrentLocationAddress } from './useCurrentLocationAddress';
+export type { UseCurrentLocationAddressParams, UseCurrentLocationAddressReturn } from './useCurrentLocationAddress';
+
